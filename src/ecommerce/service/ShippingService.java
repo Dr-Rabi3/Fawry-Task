ecc7d899e5e90ced018c9a6b71a7f742ee392f4d
Dr.rabi3 <abdalrazekmohmed6@gmail.com>
2025-07-05 23:11:44 +0300

    add quantity for shippable items

--- src/ecommerce/service/ShippingService.java
+++ src/ecommerce/service/ShippingService.java
@@ -2,13 +2,34 @@ package ecommerce.service;
 
 import ecommerce.interfaces.Shippable;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+class CountWeight {
+    int count = 0;
+    double weight = 0;
+}
 public class ShippingService {
     public static void shipItems(List<Shippable> items){
         System.out.println("\nShipping Items:");
+
+        Map<String, CountWeight> productMap = new LinkedHashMap<>();
+
         for (Shippable item : items) {
-            System.out.println("- " + item.getName() + " (" + item.getWeight() + "kg)");
+            String name = item.getName();
+            double weight = item.getWeight();
+
+            productMap.putIfAbsent(name, new CountWeight());
+            CountWeight info = productMap.get(name);
+            info.count++;
+            info.weight = weight;
+        }
+
+        for (Map.Entry<String, CountWeight> entry : productMap.entrySet()) {
+            String name = entry.getKey();
+            CountWeight info = entry.getValue();
+            System.out.printf("- %s (x%d) — %.2fkg each\n", name, info.count, info.weight);
         }
     }
 }
